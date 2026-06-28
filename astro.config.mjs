889f9fcf1af04f1cd095f711aecfb348610778cb
Radu Alexa <radu.alexa@aumovio.com>
2026-06-28 08:09:36 +0300

    Add a sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwindvite from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   // URL-ul tău final (asigură-te că domeniul alexavolt.ro este legat la acest repo)
@@ -8,6 +9,8 @@ export default defineConfig({
   // Schimbăm baza la '/' pentru că acum site-ul stă la rădăcină, 
   // nu mai este într-un subfolder precum /playground2
   base: '/',
+
+  integrations: [sitemap()],
 
   vite: {
     plugins: [tailwindvite()],
